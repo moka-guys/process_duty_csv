d286cf5d36090ce6212fa3f05bce9341068f7294
Rachel Duffin <rachel.g.duffin@gmail.com>
2023-03-03 19:56:56 +0000

    Add separate logging module

---
+++ logger.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""mokaguys_logger.py
+
+Log messages using the python standard library logging module.
+"""
+import sys
+import logging
+import config
+
+
+class Logger(object):
+    """Simple logging class
+
+    shutdown_logs()
+        To prevent duplicate filehandlers and system handlers close and
+        remove all handlers for all log files that have a python logging object
+    _get_file_handler()
+        Returns the FileHandler associated with the logging object
+    _get_syslog_handler()
+        Returns the SysLogHandler associated with the logging object
+    _get_stream_handler()
+        Returns the StreamHandler associated with the logging object
+    get_logger()
+        Return a Python logging object
+    """
+
+    _formatter = logging.Formatter(
+        config.LOGGING_FORMATTER
+    )  # Log string format
+
+    def __init__(self, logfile_path):
+        """
+        Constructor for the Logger class
+            :param logfile_path(str): Logfile path
+        """
+        self.logger = self.get_logger("logger", logfile_path)
+
+    def shutdown_logs(self):
+        """
+        To prevent duplicate filehandlers and system handlers close and
+        remove all handlers for all log files that have a python logging object
+        """
+        for handler in self.logger.handlers[:]:
+            self.logger.removeHandler(handler)
+            handler.close()
+
+    def _get_file_handler(self, filepath):
+        """
+        Returns the FileHandler associated with the logging object
+            :return file_handler (obj):   FileHandler object
+        """
+        file_handler = logging.FileHandler(filepath, mode="a", delay=True)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(self._formatter)
+        return file_handler
+
+    def _get_stream_handler(self):
+        """
+        Returns the StreamHandler associated with the logging object
+            :return stream_handler (obj):   StreamHandler object
+        """
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(logging.DEBUG)
+        stream_handler.setFormatter(self._formatter)
+        return stream_handler
+
+    def get_logger(self, name, filepath):
+        """
+        Return a Python logging object
+            :param name(str):       Logger name
+            :param filepath(str):   Logfile path
+            :return logger (obj):   Python logging object
+        """
+        logger = logging.getLogger(name)
+        logger.filepath = filepath
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(self._get_file_handler(filepath))
+        logger.addHandler(self._get_stream_handler())
+        return logger
